dae8b4ade34420d9e9865fa687e178e8abff0b60
Pogbayoe <adebayooluwasegun335@gmail.com>
2025-02-01 23:31:37 +0100

    fix: enhance CORS configuration to allow multiple origins

--- src/index.ts
+++ src/index.ts
@@ -12,13 +12,19 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 
-app.use(cors({
-    origin: 'https://d-market-3z4m.vercel.app', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+const allowedOrigins = ['https://d-market-3z4m.vercel.app', 'http://localhost:5173'];
 
-app.use(express.json());
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 mongoose
   .connect(process.env.MONGO_URL as string)
